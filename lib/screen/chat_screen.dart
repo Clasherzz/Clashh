b360998090fe6d51985b0429a23be0272f309afe
Clasherzz <govindkm20044@gmail.com>
2024-07-18 23:23:22 +0530

    working on message cards and that screen

--- lib/screen/chat_screen.dart
+++ lib/screen/chat_screen.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:flutter_application_2/models/message.dart';
 import 'package:flutter_application_2/models/user.dart';
+import 'package:flutter_application_2/widgets/message_card.dart';
 
 class ChatScreen extends StatefulWidget {
   final ChatUser user;
@@ -23,7 +25,48 @@ class _ChatScreenState extends State<ChatScreen> {
       body: Column(
         children: [
           Expanded(
-            child: 
+            child: StreamBuilder(
+    stream: null ,
+      builder: (context,snapshot){
+        switch(snapshot.connectionState){
+          case ConnectionState.waiting:
+          case ConnectionState.none:
+          //  print("after none\n");
+            return Center(child:CircularProgressIndicator());
+          
+          case ConnectionState.active:
+          case ConnectionState.done:
+           List<Message> lists = [];
+       
+        // if(snapshot.hasData){
+        //    final data = snapshot.data?.docs;
+        //   lists = data?.map((e) => ChatUser.fromJson(e.data()),).toList() ?? [];
+        // }
+        if(lists.isNotEmpty){
+        return ListView.builder(
+          itemCount: lists.length,
+          itemBuilder: (context,index){
+            // return Text('name : ${lists[index]}');
+            return MessageCard(message: lists[index]);
+          },);}else{
+            return const Center(child: Text("No Messages"));
+          }
+
+           
+        }
+
+
+       
+      },
+
+      // body: Column(
+      //   children: [
+      //     ChatUser(),
+      //     ChatUser(),
+      //   ],
+      
+      
+      ),
           
           
           ),
--- lib/widgets/message_card.dart
+++ lib/widgets/message_card.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/widgets.dart';
+import 'package:flutter_application_2/models/message.dart';
 
 class MessageCard extends StatefulWidget {
-  const MessageCard({super.key});
+  final Message message;
+  const MessageCard({super.key,required this.message});
 
   @override
   State<MessageCard> createState() => _MessageCardState();
@@ -12,5 +14,21 @@ class _MessageCardState extends State<MessageCard> {
   @override
   Widget build(BuildContext context) {
     return const Placeholder();
+  }
+
+  Widget _blueMessage(){
+    return Row(children: [
+      Container(
+        
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.only(
+            topLeft:Radius.circular(30) ,
+            topRight: Radius.circular(30),
+            bottomRight: Radius.circular(30),
+          )
+        ),
+        child:Text(widget.message.msg)
+      )
+    ],)
   }
 }
